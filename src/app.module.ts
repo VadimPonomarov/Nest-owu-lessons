9d2a1a8fc014df7272c6ae11401ffeb5a2444b6e
Вадим <pvs.versia@gmail.com>
2022-06-09 21:42:03 +0300

    Commit 1

---
+++ src/app.module.ts
@@ -0,0 +1,25 @@
+import {Module} from '@nestjs/common';
+import {ConfigModule} from '@nestjs/config';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+
+import {configuration} from './config';
+import {UserModule} from './modules/user/user.module';
+import {UserController} from "./modules/user/user.controller";
+import {UserService} from "./modules/user/user.service";
+import {PrismaClient} from "@prisma/client";
+import {PrismaService} from "./core/prisma.service";
+import {PostModule} from './modules/post/post.module';
+import {CommentModule} from './modules/comment/comment.module';
+
+
+@Module({
+    imports: [ConfigModule.forRoot({
+        isGlobal: true,
+        load: [configuration],
+    }), UserModule, PrismaClient, PostModule, CommentModule],
+    controllers: [AppController, UserController],
+    providers: [AppService, UserService, PrismaService],
+})
+export class AppModule {
+}
---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import {NestFactory} from '@nestjs/core';
+import {AppModule} from './app.module';
+
+import {ConfigService} from '@nestjs/config';
+import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger";
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const appConfig = await app.get(ConfigService);
+    const PORT = appConfig.get('port');
+
+    const swagConfig = new DocumentBuilder()
+        .setTitle('Nest-OWU-lesons')
+        .setDescription('Nest-OWU-lesons api description')
+        .setVersion('1.0')
+        .addTag('Nest-OWU-lesons')
+        .build();
+    const document = SwaggerModule.createDocument(app, swagConfig);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(PORT, () => {
+        console.log('Server has started on port: ' + PORT);
+    });
+}
+
+bootstrap();
