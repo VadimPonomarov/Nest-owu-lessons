663739196c38369c3495663eab67c0eeac35f9b9
Вадим <pvs.versia@gmail.com>
2022-06-14 15:01:42 +0300

    Tokens + authorisation + refresh + bearer 1

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,6 +1,6 @@
 export const configuration = () => ({
     production: false,
-    port: parseInt(process.env.PORT, 10) || 3000,
-    token_secret: process.env.TOKEN_SECRET || 'my_token_secret_word',
-    salt: process.env.BCRYPT_SALT || 5
+    port: 3000,
+    token_secret: 'my_token_secret_word',
+    salt: 5
 });
--- src/modules/token/token.service.ts
+++ src/modules/token/token.service.ts
@@ -1,65 +1,88 @@
-import {ForbiddenException, Injectable} from '@nestjs/common';
+import {ForbiddenException, HttpException, HttpStatus, Injectable} from '@nestjs/common';
+import {JwtService} from "@nestjs/jwt";
 
 import {CreateTokenDto} from './dto/create-token.dto';
 import {PrismaService} from "../../core/prisma.service";
 import {CreateUserDto} from "../user/dto/create-user.dto";
 import {ITokenPair} from "./interfaces/token-pair.interface";
-import {JwtService} from "@nestjs/jwt";
-import {ConfigService} from "@nestjs/config";
+import {configuration} from "../../config";
 
 @Injectable()
 export class TokenService {
     constructor(private _prismaService: PrismaService,
-                private _jwtService: JwtService,
-                private _configService: ConfigService) {
+                private _jwtService: JwtService) {
     }
 
     async create(createTokenDto: CreateTokenDto) {
-        return await this._prismaService.token.create({data: {token: createTokenDto.token}});
+        try {
+            return await this._prismaService.token.create({data: {token: createTokenDto.token}});
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+        }
     }
 
     async findOne(token: string) {
-        return await this._prismaService.token.findUnique({where: {token: token}});
+        try {
+            return await this._prismaService.token.findUnique({where: {token: token}});
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+        }
     }
 
     async remove(token: string) {
-        return await this._prismaService.token.delete({where: {token}});
+        try {
+            return await this._prismaService.token.delete({where: {token}});
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+        }
     }
 
     getTokenPair(user: Partial<CreateUserDto>): ITokenPair {
-        const payLoad = {name: user.name, email: user.email};
-        const accessToken = this._jwtService.sign(payLoad,
-            {
-                secret: process.env.TOKEN_SECRET,
-                expiresIn: '15m'
-            });
-        const refreshToken = this._jwtService.sign(payLoad,
-            {
-                secret: process.env.TOKEN_SECRET,
-                expiresIn: '30d'
-            });
-        return {accessToken, refreshToken};
+        try {
+            const payLoad = {name: user.name, email: user.email};
+            const accessToken = this._jwtService.sign(payLoad,
+                {
+                    secret: configuration().token_secret,
+                    expiresIn: '15m'
+                });
+            const refreshToken = this._jwtService.sign(payLoad,
+                {
+                    secret: configuration().token_secret,
+                    expiresIn: '30d'
+                });
+            return {accessToken, refreshToken};
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+        }
     }
 
     async refreshTokenPair(refreshToken: string): Promise<ITokenPair> {
-        const tokenDAta = await this._jwtService.verify(refreshToken,
-            {
-                secret: process.env.TOKEN_SECRET
-            });
+        try {
+            const tokenDAta = await this._jwtService.verify(refreshToken,
+                {
+                    secret: configuration().token_secret
+                });
 
-        if (!tokenDAta) {
-            throw new ForbiddenException();
+            if (!tokenDAta) {
+                throw new ForbiddenException();
+            }
+            const {name, email} = tokenDAta;
+            return await this.getTokenPair({name, email});
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
         }
-        const {name, email} = tokenDAta;
-        return await this.getTokenPair({name, email});
     }
 
     isTokenValid(token: string): boolean {
-        const isTokenValid = this._jwtService.verify(token,
-            {
-                secret: process.env.TOKEN_SECRET
-            }
-        );
-        return isTokenValid;
+        try {
+            const isTokenValid = this._jwtService.verify(token,
+                {
+                    secret: configuration().token_secret
+                }
+            );
+            return isTokenValid;
+        } catch (e) {
+            throw new HttpException('Something went wrong', HttpStatus.BAD_REQUEST);
+        }
     }
 }
